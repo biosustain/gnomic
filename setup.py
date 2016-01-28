0ab8277c04e893c0ca83059f3c92ada68f310a19
Lars Schöning <lars@lyschoening.de>
2016-01-28 11:58:38 +0100

    chore: release version 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gnomic',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(exclude=['*tests*']),
     license='Apache',
     author='Lars Schöning',
