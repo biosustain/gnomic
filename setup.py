c16792a11ecec3e5a49cb4f61af3cae486832e16
Lars Schöning <lars@lyschoening.de>
2016-11-16 15:34:50 +0100

    chore: release 0.2.1 with Python 2.7 support

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gnomic',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(exclude=['*tests*']),
     license='Apache',
     author='Lars Schöning',
