af701054b45f3b180cc4e4bb117f277a96523f45
Melanie-M <melmor@biosustain.dtu.dk>
2017-07-31 10:21:00 +0200

    feat: add genotype_to and change_to util functions

--- gnomic/utils.py
+++ gnomic/utils.py
@@ -1,4 +1,5 @@
 from gnomic import Genotype
+from gnomic.formatters import BUILTIN_FORMATTERS
 
 
 def chain(*gnomic_strings, parent=None, **kwargs):
@@ -6,3 +7,43 @@ def chain(*gnomic_strings, parent=None, **kwargs):
     for gnomic_string in gnomic_strings[1:]:
         genotype = Genotype.parse(gnomic_string, parent=genotype, **kwargs)
     return genotype
+
+
+def genotype_to_string(genotype):
+    return BUILTIN_FORMATTERS['gnomic'].format_genotype(genotype)
+
+
+def genotype_to_text(genotype):
+    return BUILTIN_FORMATTERS['text'].format_genotype(genotype)
+
+
+def genotype_to_html(genotype):
+    return BUILTIN_FORMATTERS['html'].format_genotype(genotype)
+
+
+def change_to_string(change):
+    return BUILTIN_FORMATTERS['gnomic'].format_change(change)
+
+
+def change_to_text(change):
+    return BUILTIN_FORMATTERS['text'].format_change(change)
+
+
+def change_to_html(change):
+    return BUILTIN_FORMATTERS['html'].format_change(change)
+
+
+def feature_to_string(feature):
+    return BUILTIN_FORMATTERS['gnomic'].format_feature(feature)
+
+
+def feature_to_text(feature):
+    return BUILTIN_FORMATTERS['text'].format_feature(feature)
+
+
+def feature_to_html(feature):
+    return BUILTIN_FORMATTERS['html'].format_feature(feature)
+
+
+def get_genotype_change_strings(genotype):
+    return [change_to_string(change).lower() for change in genotype.changes()]
